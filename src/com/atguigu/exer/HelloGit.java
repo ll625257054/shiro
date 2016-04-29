5edbc3f71ac38e489af9d3f6a4ca7050241d208c
lilong <625257054@qq.com>
2016-04-29 20:46:07 +0800

    update 02

--- src/com/atguigu/exer/HelloGit.java
+++ src/com/atguigu/exer/HelloGit.java
@@ -4,6 +4,8 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		System.out.println("hello git");
+		System.out.println("another progammer's code");
+		System.out.println("124");
 	}
 	
 }
